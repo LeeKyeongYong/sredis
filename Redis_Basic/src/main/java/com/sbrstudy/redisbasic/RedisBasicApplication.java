3f19e2404ccfe9ca2dfb02379289cdd1bd814b45
이경용 <sleekydz86@icloud.com>
2023-12-20 23:45:44 +0900

    기본클래스 생성
    기본클래스 생성

---
+++ Redis_Basic/src/main/java/com/sbrstudy/redisbasic/RedisBasicApplication.java
@@ -0,0 +1,13 @@
+package com.sbrstudy.redisbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisBasicApplication.class, args);
+    }
+
+}
---
+++ Redis_Basic/src/test/java/com/sbrstudy/redisbasic/RedisBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbrstudy.redisbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
